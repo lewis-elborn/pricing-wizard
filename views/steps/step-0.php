6b88696f5b412f94f038cb22181a0a749c4f12af
Lewis Elborn <lewis.elborn@aphinity.co.uk>
2022-06-03 20:45:50 +0100

    Update step-0.php

--- views/steps/step-0.php
+++ views/steps/step-0.php
@@ -1,16 +1,16 @@
-<div class="pw-step" data-step="<?php echo esc_attr($args['id']); ?>" data-ajax-action="verifyPostcode">
-    <div class="pw-step__inner">
-        <p class="pw-step__title">Let's get you underway...</p>
+<div class="apwb-step" data-curStep="<?php echo esc_attr($args['id']); ?>" data-xhrAction="verifyPostcode">
+    <div class="apwb-step__inner">
+        <p class="apwb-step__title">Let's get you underway...</p>
 
-        <div class="pw-fields">
-            <div class="pw-field">
-                <label for="postCode" class="pw-field__label">Enter your postcode</label>
-                <input type="text" placeholder="E.g. AL1 2BQ" id="postCode" data-cf-name="postCode">
+        <div class="apwb-fields">
+            <div class="apwb-field">
+                <label for="postCode" class="apwb-field__label">Enter your postcode</label>
+                <input type="text" placeholder="E.g. AL1 2BQ" id="postCode" class="apwb-postcode">
             </div>
         </div>
     </div>
 
-    <div class="pw-buttons">
-        <button class="pw-button" data-direction="next">Continue</button>
+    <div class="apwb-btns">
+        <button class="apwb-btn" data-dir="next">Continue</button>
     </div>
 </div>
