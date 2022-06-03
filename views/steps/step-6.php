68ef77c85d78672e658e91b41727497c67e37832
Lewis Elborn <lewis.elborn@aphinity.co.uk>
2022-06-03 20:45:59 +0100

    Update step-6.php

--- views/steps/step-6.php
+++ views/steps/step-6.php
@@ -1,11 +1,11 @@
-<div class="pw-step" data-step="<?php echo esc_attr($args['id']); ?>">
-    <div class="pw-step__inner">
-        <p class="pw-step__title">Thanks, we've got it.</p>
+<div class="apwb-step" data-step="<?php echo esc_attr($args['id']); ?>">
+    <div class="apwb-step__inner">
+        <p class="apwb-step__title">Thanks, we've got it.</p>
 
         <p>We've got everything in hand. You should receive a confirmation email shortly. After which, someone will be in touch to discuss your lessons.</p>
     </div>
 
-    <div class="pw-buttons">
-        <a class="pw-button" href="<?php echo esc_url(get_home_url()); ?>">Home</a>
+    <div class="apwb-btns">
+        <a class="apwb-btn" href="<?php echo esc_url(get_home_url()); ?>">Home</a>
     </div>
 </div>
