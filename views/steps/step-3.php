390f955b964bb9ec75d795b0c4ca6f2c16743f08
Lewis Elborn <lewis.elborn@aphinity.co.uk>
2022-06-03 20:45:56 +0100

    Update step-3.php

--- views/steps/step-3.php
+++ views/steps/step-3.php
@@ -1,22 +1,22 @@
-<div class="pw-step" data-step="<?php echo esc_attr($args['id']); ?>">
-    <div class="pw-step__inner">
-        <p class="pw-step__title">Awesome! Next up...</p>
+<div class="apwb-step" data-step="<?php echo esc_attr($args['id']); ?>">
+    <div class="apwb-step__inner">
+        <p class="apwb-step__title">Awesome! Next up...</p>
         
         <p>Which gearbox would you prefer?</p>
 
-        <div class="pw-field pw-field--radio">
+        <div class="apwb-field apwb-field--radio">
             <input type="radio" id="manualGearbox" name="typeOfGearbox" value="Manual Gearbox" data-cf-name="typeOfGearbox" checked>
             <label for="manualGearbox">Manual Gearbox</label>
         </div>
 
-        <div class="pw-field pw-field--radio">
+        <div class="apwb-field apwb-field--radio">
             <input type="radio" id="autoGearbox" name="typeOfGearbox" value="Automatic Gearbox" data-cf-name="typeOfGearbox">
             <label for="autoGearbox">Automatic Gearbox</label>
         </div>
     </div>
 
-    <div class="pw-buttons">
-        <button class="pw-button" data-direction="previous">Go Back</button>
-        <button class="pw-button" data-direction="next">Continue</button>
+    <div class="apwb-btns">
+        <button class="apwb-btn" data-direction="previous">Go Back</button>
+        <button class="apwb-btn" data-direction="next">Continue</button>
     </div>
 </div>
